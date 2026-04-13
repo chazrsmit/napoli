5a2a1dffbe4be83366d61471a52925267044571f
Charlotte <smit-charlotte@outlook.com>
2026-04-13 20:18:11 +0200

    titre bottom bar

---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,15 @@
+import './bottombar.css';
+import titre2 from '../assets/elements/titre-pink2.png';
+
+export default function BottomBar() {
+
+
+    return(
+
+        <>
+        <div className="bottombar">
+            <img src={`${titre2}`} />
+        </div>
+        </>
+    )
+}
