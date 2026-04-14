85286737a4037733011c801b1af0943118e03527
Charlotte <smit-charlotte@outlook.com>
2026-04-14 22:37:07 +0200

    croix close

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,34 @@
 import './modal.css'
+import { useState } from 'react'
+import close from '../assets/elements/close.png'
+import close2 from '../assets/elements/close-contour.png'
 
-export default function Modal({ children, onClose }) {
+export default function Modal({ children, onClose, isModalOpen }) {
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const whenHovered = () => {
+        setIsHovered(!isHovered)
+        console.log(isHovered)
+    }
+
+    const notHovered = () => {
+        setIsHovered(!isHovered)
+        console.log(isHovered)
+    }
 
     return (
 
         <>
-        <div className="overlay">
+        <div className={`overlay ${isModalOpen? 'opened' : 'closed'}`}>
             <div className="modal">
                 <div className="texte">
                     {children}
-                    <button className="closeBtn" onClick={onClose}>
-                        close
-                    </button>
                 </div>
+                <button className="closeBtn" onClick={onClose}>
+                    <img src={`${close2}`} className={`close2 ${isHovered? 'hovered' : 'notHovered'}`}/>
+                    <img src={`${close}`} onMouseEnter={whenHovered} onMouseLeave={notHovered} className={`close1 ${isHovered? 'hovered' : 'notHovered'}`} />
+                </button>
             </div>
         </div>
         </>
